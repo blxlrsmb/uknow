280e9afd923182f10e4bbb9cbe6a0cd1f82a486e
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-10-10 22:28:20 +0800

    website flask framework

---
+++ unknown-website/unknown/utils/__init__.py
@@ -0,0 +1,24 @@
+import os
+import os.path
+
+from flask import Flask
+
+
+class DefaultConfig(object):
+    HOST = None
+    PORT = None
+    RUN_OPTIONS = {}
+    DEBUG = True
+
+
+def create_app(subproject):
+    app = Flask('unknown.%s' % subproject, instance_relative_config=True,
+                instance_path=os.environ.get('UNKNOWN_INSTANCE'))
+    app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+    app.config.from_object(DefaultConfig())
+    try:
+        app.config.from_pyfile(os.path.join('config', '%s.py' % subproject))
+    except IOError:
+        # TODO use logging module
+        print 'WARNING: No configuration found, using builtin defaults.'
+    return app
---
+++ unknown-website/unknown/website/app.py
@@ -0,0 +1,15 @@
+# encoding=utf-8
+from unknown.utils import create_app
+from .apps.base import base
+
+
+def create_website_app():
+    app = create_app('website')
+    app.register_blueprint(base)
+    return app
+
+
+def main():
+    app = create_website_app()
+    app.run(app.config['HOST'], app.config['PORT'],
+            **app.config['RUN_OPTIONS'])
