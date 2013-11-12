3ca017ee3c53f52f80be2fb54c359658d4dbd44e
jiakai <jia.kai66@gmail.com>
2013-11-13 01:47:37 +0800

    added todo

--- fetcher/ukfetcher/__init__.py
+++ fetcher/ukfetcher/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: __init__.py
-# $Date: Wed Nov 13 01:13:38 2013 +0800
+# $Date: Wed Nov 13 01:47:24 2013 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 """provide fetcher background service, and also utilities for managing
@@ -9,3 +9,4 @@ fetchers"""
 
 from .user import get_celery_task as get_user_fetcher_celery_task
 
+# TODO: limit user connection; coroutine for different users
