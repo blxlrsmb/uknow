e8a7e1f47e1d5a6fdc8e10f50f00a10ec21fde6c
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-12-12 21:42:22 +0800

    fix china daily database

--- fetcher/general_fetcher_server.py
+++ fetcher/general_fetcher_server.py
@@ -1,7 +1,7 @@
 #!../manage/exec-in-virtualenv.sh
 # -*- coding: utf-8 -*-
 # $File: general_fetcher_server.py
-# $Date: Wed Nov 13 01:01:24 2013 +0800
+# $Date: Thu Dec 12 21:18:58 2013 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 from gevent import monkey
@@ -9,4 +9,10 @@ monkey.patch_all()
 
 from ukfetcher.general import start_server
 
-start_server()
+
+def main():
+    start_server()
+
+
+if __name__ == "__main__":
+    main()
--- manage/setup.py
+++ manage/setup.py
@@ -3,26 +3,32 @@ from setuptools import setup
 setup_args = dict(
     name='uknow',
     install_requires=[
+        # web framework
         'Flask>=0.9',
         'Flask-WTF>=0.8',
         'Flask-Login>=0.2.7',
         'pyjade>=1.6',
 
+        # database
         'pymongo>=2.6.3',
         'celery>=3.1.1',
         'redis>=2.8.0',
         'gevent>=0.13.8',
+
+        # tool
         'sphinx>=1.1.3',
         'pep8>=1.4.6',
-        'feedparser==5.1.3',
         'termcolor>=1.1.0',
+
+        # misc
+        'feedparser==5.1.3',
         'BeautifulSoup>=3.2.1',
         'requests>=2.1.0'
     ],
     entry_points=dict(
         console_scripts=[
             'api-website = standalone_server:main',
-            'fetcher-server = general_fetcher_server',
+            'fetcher-server = general_fetcher_server:main',
             'frontend-website = ukfrontend.app:main',
             ],
     ),
