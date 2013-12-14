1b6b65f599489eca4907f4b1ad60c0752bf59326
vera <jnchen2011@163.com>
2013-12-14 14:06:53 +0800

    change api-website coverage test scope

--- tests/test_api_website_coverage.sh
+++ tests/test_api_website_coverage.sh
@@ -16,13 +16,13 @@ source ../manage/setenv.sh
 python2 -m coverage run ../api-website/standalone_server.py > /tmp/test_api_website.log &
 SERVER_PID=$!
 
-count_down 5
+count_down 3
 
 ./test_api_website.py
 RET_VAL=$?
 kill -s SIGINT $SERVER_PID
 [ "x$RET_VAL" != "x0" ] && exit 1
 
-python2 -m coverage html --omit="*.env/*" -d $REPORT_DIR
+python2 -m coverage html --include="*manage/config,*api-website*" --omit="*common/lib*" -d $REPORT_DIR
 python2 -m coverage erase
 $BROWSER $REPORT_DIR/index.html
