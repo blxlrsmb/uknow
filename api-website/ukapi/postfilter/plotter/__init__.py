c96192af79df2351508daff07e53eb8079421ddb
jiakai <jia.kai66@gmail.com>
2013-12-15 00:40:03 +0800

    plotter done

--- api-website/ukapi/postfilter/plotter/__init__.py
+++ api-website/ukapi/postfilter/plotter/__init__.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: __init__.py
-# $Date: Sat Dec 14 22:36:18 2013 +0800
+# $Date: Sun Dec 15 00:39:59 2013 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
+
+"""mongo doc: collection: plotter_cache
+{
+    _id: key
+    v: value
+}
+"""
 
 from .. import PostfilterBase
 from ...api import app
+
+from ukitem import TextOnlyItem
+from ukutil import MongoDBLRUCache
 
 import flask
 from flask import request
 
 import re
 import os.path
+import calendar
+import uuid
+
+from cStringIO import StringIO
+
+_cache = MongoDBLRUCache(3600, 'postfilter_plotter_val')
+
+
+def _render_html(xdata, ydata):
+    with open(os.path.join(os.path.dirname(__file__), 'plot.html')) as fin:
+        content = fin.read()
+    data = '[' + ','.join('[{},{}]'.format(i[0], i[1])
+                          for i in zip(xdata, ydata)) + ']'
+    content = content. \
+        replace('__URL__', '/postfilter_plotter'). \
+        replace('__DATA__', data)
+
+    return content
 
 
 class Plotter(PostfilterBase):
@@ -35,15 +63,18 @@ class Plotter(PostfilterBase):
         self._del_config(user_id)
 
     def apply(self, ctx, docs):
-        conf = self._load_config()
+        conf = self._load_config(ctx.user_id)
         reobj = re.compile(conf['reg_exp'])
         tag = conf['tag']
-        newest_time = None
+        tag_lower = tag.lower()
+        doc_newest = None
         idx = 0
+        data_x = []
+        data_y = []
         while idx < len(docs):
             cur = docs[idx]
             try:
-                assert tag in cur['tag']
+                assert tag_lower in [i.lower() for i in cur['tag']]
                 value = float(
                     reobj.match(cur['desc'].render_title() + '\n' +
                                 cur['desc'].render_content()).group(1))
@@ -52,22 +83,54 @@ class Plotter(PostfilterBase):
                 continue
 
             time = cur['creation_time']
-            if not newest_time or time > newest_time:
-                newest_time = time
+            if not doc_newest or time > doc_newest['creation_time']:
+                doc_newest = cur
+
+            data_x.append(calendar.timegm(time.utctimetuple()) * 1e3)
+            data_y.append(value)
+
+            del docs[idx]
+
+        if doc_newest:
+            pageid = str(uuid.uuid4())
+            _cache.put(pageid, _render_html(data_x, data_y))
+            doc_newest['tag'] = ['plotter-' + tag]
+            doc_newest['desc'] = TextOnlyItem(
+                u'走势曲线：' + tag,
+                """<iframe
+                    style="border:none; width:800px; height:600px;"
+                    src="/postfilter_plotter?v={}"></iframe>""".
+                format(pageid))
+            docs.append(doc_newest)
 
 
 @app.route('/postfilter_plotter')
 def file_server():
-    q = request.values.get('q')
+    v = request.values.get('v')
+    fcontent = None
+    fsrc = None
+    if v:
+        fcontent = _cache.get(v, 'not found')
+    else:
+        q = request.values.get('q')
 
-    filemap = {
-        'jflot.js': 'jquery.flot.min.js',
-        'jquery.js': 'jquery.min.js',
-        'jflot.time.js': 'jquery.flot.time.min.js',
-        'style.css': 'examples.css'
-    }
+        filemap = {
+            'jflot.js': 'jquery.flot.min.js',
+            'jquery.js': 'jquery.min.js',
+            'jflot.time.js': 'jquery.flot.time.min.js',
+            'jflot.selection.js': 'jquery.flot.selection.min.js'
+        }
 
-    fname = filemap.get(q, 'x.html')
-
-    return flask.send_file(
-        os.path.join(os.path.dirname(__file__), fname))
+        fname = filemap.get(q)
+        if fname:
+            fsrc = os.path.join(os.path.dirname(__file__), fname)
+            mimetype = None
+    if fsrc is None:
+        if fcontent is None:
+            fcontent = _render_html(
+                range(10**5, 9 * 10**5, 10**4), range(0, 100))
+        fsrc = StringIO()
+        fsrc.write(fcontent)
+        fsrc.seek(0)
+        mimetype = 'text/html'
+    return flask.send_file(fsrc, mimetype=mimetype)
--- common/ukutil.py
+++ common/ukutil.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: ukutil.py
-# $Date: Sat Dec 14 19:13:34 2013 +0800
+# $Date: Sat Dec 14 23:52:56 2013 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 """common utility functions"""
@@ -10,6 +10,7 @@ from ukdbconn import get_mongo, DuplicateKeyError
 
 from importlib import import_module
 from pkgutil import walk_packages
+from datetime import datetime
 import os
 
 
@@ -63,3 +64,37 @@ class MongoDBSet(object):
         if rst:
             return list(rst[0]['v'])
         return []
+
+
+class MongoDBLRUCache(object):
+    """naive LRU cache in mongodb
+    doc: {
+        _id: key
+        v: value
+        t: creation time, indexed, expire
+    }
+    """
+
+    expire_time = None
+    coll_name = None
+
+    def __init__(self, expire_time, coll_name):
+        self.expire_time = expire_time
+        self.coll_name = coll_name
+
+    def get_coll(self):
+        return get_mongo(self.coll_name)
+
+    def put(self, key, value):
+        coll = self.get_coll()
+        coll.save({'_id': key, 'v': value,
+                   't': datetime.utcnow()})
+        coll.ensure_index('t', expireAfterSeconds=self.expire_time)
+
+    def get(self, key, default=None):
+        coll = self.get_coll()
+        rst = list(coll.find({'_id': key}))
+        if rst:
+            coll.update({'_id': key}, {'$set': {'t': datetime.utcnow()}})
+            return rst[0]['v']
+        return default
